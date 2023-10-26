e15d579482d8ed23b7aaaffe46484487f7b131f7
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2023-10-27 01:49:09 +0300

    asddas

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("MyPolicy");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
