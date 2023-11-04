7252ff94cd9544bf6e3ef303caac70dfd7849153
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2023-11-04 18:38:20 +0200

    das

--- DatabaseLayer/ProfileController.cs
+++ DatabaseLayer/ProfileController.cs
@@ -1,15 +1,16 @@
-﻿using System;
-
-public class Class1
+﻿namespace myFirstAppSol.DatabaseLayer
 {
-	public Class1()
-	{
+    public class ProfileController
+    {
+
+        private const string tableName = "Profiles";
+        private const string ColEmail = "Email";
+        private const string ColCoins = "Coins";
+        private const string ColOwnedAvatars = "OwnedAvatars";
+        private const string ColCurrAvatar="CurrAvatar";
+        private const string ColAddedDates = "AddedDates";
 
 
 
-
-
-		
-
-	}
+    }
 }
