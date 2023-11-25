342ddb0edaa4b89223ac3fa67f17640893b4ab84
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2023-11-25 03:24:00 +0200

    asd

--- LogicLayer/BoardFolder/Board.cs
+++ LogicLayer/BoardFolder/Board.cs
@@ -159,6 +159,7 @@ namespace WebApplication2.LogicLayer.BoardFolder
             {
                 if(task.Id== taskId) return task;
             }
+            return null;
         }
         //notes ---------------------------------------
 
